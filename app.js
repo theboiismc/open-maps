5eda5776f3e3142ecae02eec24ad2ccf0b5fda28
gammagreengiant <93876177+OvalCucumber97@users.noreply.github.com>
2025-07-02 17:00:33 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const map = new maplibregl.Map({
   container: 'map',
-  style: 'https://demotiles.maplibre.org/style.json',
+  style: 'https://tiles.stadiamaps.com/styles/osm-bright.json',
+
   center: [0, 20],
   zoom: 2
 });
