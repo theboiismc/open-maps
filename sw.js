27d1e7df70fc7d586516c31467579e9ba9d57969
gammagreengiant <93876177+OvalCucumber97@users.noreply.github.com>
2025-07-25 20:29:08 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,76 @@
+const CACHE_NAME = 'theboiismc-maps-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/app.js',
+  'https://unpkg.com/maplibre-gl@4.1.0/dist/maplibre-gl.css',
+  'https://unpkg.com/maplibre-gl@4.1.0/dist/maplibre-gl.js',
+  '/satelite_style.png',
+  '/default_style.png',
+  '/icon-192x192.png',
+  '/icon-512x512.png'
+];
+
+// Install event: opens a cache and adds the core files to it.
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache and caching core files');
+        return cache.addAll(urlsToCache);
+      })
+  );
+  self.skipWaiting();
+});
+
+// Fetch event: serves cached content when offline.
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response from cache
+        if (response) {
+          return response;
+        }
+
+        // Not in cache - fetch from network, then cache it
+        return fetch(event.request).then(
+          networkResponse => {
+            // Check if we received a valid response
+            if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic') {
+              return networkResponse;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            const responseToCache = networkResponse.clone();
+
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return networkResponse;
+          }
+        );
+      })
+  );
+});
+
+// Activate event: removes old caches.
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
