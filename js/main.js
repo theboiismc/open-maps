6cad78bb373f2a6f955a502ca880e66e2c6364a2
gammagreengiant <93876177+OvalCucumber97@users.noreply.github.com>
2025-09-01 12:42:26 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 /* ========= INIT ========= */
 import { initMap } from './map.js';
 import { loadPlaces } from './data.js';
+import './theme.js'; // ensure theme loads
+import './auth.js';  // load auth config if needed
 
 document.addEventListener("DOMContentLoaded", () => {
     initMap();
     loadPlaces();
 });
-
