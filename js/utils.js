8d4c6ca2cfcb824e1708a2cb275a16796e6d8d9f
gammagreengiant <93876177+OvalCucumber97@users.noreply.github.com>
2025-09-02 13:04:03 -0500

    Create utils.js

---
+++ js/utils.js
@@ -0,0 +1,2 @@
+// --- UTILITY FUNCTIONS ---
+function debounce(func, delay) { let timeout; return function(...args) { clearTimeout(timeout); timeout = setTimeout(() => func.apply(this, args), delay); }; }
