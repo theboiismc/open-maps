7d41ad97da3c9324d2f768802cb2c5c4d9a19a33
gammagreengiant <93876177+OvalCucumber97@users.noreply.github.com>
2025-09-03 21:19:13 -0500

    Update map.js

--- js/map.js
+++ js/map.js
@@ -12,13 +12,6 @@ const map = new maplibregl.Map({
     container: "map",
     style: STYLES.default,
     center: [-95, 39],
-    zoom: 2,
-    pitch: 0, // no tilt
-    bearing: 0,
-    dragRotate: false,      // disable mouse drag rotation
-    touchPitch: false,      // disable pinch-tilt gesture
-    pitchWithRotate: false, // disables right-click+drag tilt
-    renderWorldCopies: false
 });
 
 
